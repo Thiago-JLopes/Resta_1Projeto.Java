df2d1cb06ac25cc1b4900bc108ff7e991a22133b
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2022-11-01 23:14:58 -0300

    Adiciona regras e instruções do jogo.

--- src/main/java/dcc025/resta1/Resta1.java
+++ src/main/java/dcc025/resta1/Resta1.java
@@ -2,16 +2,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package dcc025.resta1;
 
 /**
  *
  * @author thiago
  */
+import java.util.Scanner;
+
 public class Resta1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("----------------------------------------------------------------------------------\n                                    RESTA 1");
+        System.out.println("----------------------------------------------------------------------------------");
+
+        System.out.println("\n\nREGRAS DO JOGO:");
+        System.out.println("""
+                           1 - Para eliminar uma peca voce precisa pular sobre ela, como no jogo de Damas.
+                           2 - O movimento deve ser sempre na horizontal ou na vertical, terminando em um
+                           espaco vazio.
+                           3 - A peça que foi "saltada" é retirada do tabuleiro. O jogo
+                           termina quando não é mais possível fazer nenhum outro movimento.
+                           4 - Você ganha o jogo quando resta apenas uma peca.""");
+
+        System.out.println("\nCOMO JOGAR:");
+        System.out.println("""
+                           Para fazer uma jogada, entre com o seguinte formato [linha da peca], [coluna da peca], [linha do espaco em
+                           branco], [coluna do espaco em branco].""");
+
+        System.out.println("\n\n\nBORA JOGAR!!");
+    }  
 }
